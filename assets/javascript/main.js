f4b79a21a4b774be9005b9bd3207721bd0eab0ab
markeemoon <markdreed512@gmail.com>
2019-02-18 11:57:31 -0600

    portfolio: card backs started

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,5 +2,10 @@ $('#dropdown-icon').on('click', function(){
     $('#dropdown-container').slideToggle()
 })
 
+$('button').on('click', function(){
+    console.log('clicked')
+    $('#main').addClass('slideOut')
+})
+
 
     
